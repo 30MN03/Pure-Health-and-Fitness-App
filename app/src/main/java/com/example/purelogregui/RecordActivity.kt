e56538b96028fdb23668583619051efb88a00e7e
44740 <30mn03@gmail.com>
2021-02-28 20:22:14 +0000

    Current progress

--- app/src/main/java/com/example/purelogregui/RecordActivity.kt
+++ app/src/main/java/com/example/purelogregui/RecordActivity.kt
@@ -1,13 +1,23 @@
 package com.example.purelogregui
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_record.*
+import kotlinx.android.synthetic.main.activity_review.*
+import kotlinx.android.synthetic.main.activity_review.btnSaveInfo
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.*
 
-class RecordActivity {
+
+class RecordActivity : AppCompatActivity() {
+    //Authenticate Firebase
     lateinit var auth: FirebaseAuth
     var databaseReference: DatabaseReference? = null
     var database: FirebaseDatabase? = null
@@ -16,6 +26,7 @@ class RecordActivity {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
 
+        //Access database table named "Users"
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
         databaseReference = database?.reference!!.child("Users")
@@ -23,11 +34,20 @@ class RecordActivity {
         record()
     }
 
-    private fun record() {
 
-        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
-        val currentDate = sdf.format(Date())
-        println(" C DATE is  $currentDate")
-        et_recordDate
+    val date: TextView = findViewById(R.id.tv_recordDate)
+    date.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+
+    //Retrieve information
+    private  fun record() {
+
+
+
+
+        btnRecord.setOnClickListener {
+            startActivity(Intent(this@RecordActivity, HomeActivity::class.java))
+            finish()
+        }
     }
 }
