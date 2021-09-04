53da8df7d90d3759eb6c9f92ddaebcdc875ed27f
44740 <30mn03@gmail.com>
2021-09-04 18:37:29 +0100

    Final changes

--- app/src/main/java/com/example/purelogregui/RecordActivity.kt
+++ app/src/main/java/com/example/purelogregui/RecordActivity.kt
@@ -2,6 +2,7 @@ package com.example.purelogregui
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -32,7 +33,7 @@ class RecordActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
         databaseReference = database?.reference!!.child("Users")
-        databaseReference2 =database?.reference!!.child("Records")
+        databaseReference2 = database?.reference!!.child("Records")
 
         record()
     }
@@ -49,19 +50,30 @@ class RecordActivity : AppCompatActivity() {
 
 
         btnRecord.setOnClickListener {
-            currentUserDb?.child("Date")?.setValue(date.text.toString())
-            currentUserDb?.child("Weight")?.setValue(et_recordWeight.text.toString())
-            currentUserDb?.child("Time")?.setValue(et_recordTime.text.toString())
-            currentUserDb?.child("Calories")?.setValue(et_recordCalories.text.toString())
+             if (TextUtils.isEmpty(et_recordWeight.text.toString())) {
+                    et_recordWeight.error = "Please enter your weight"
+                    return@setOnClickListener
+                } else if (TextUtils.isEmpty(et_recordTime.text.toString())) {
+                    et_recordTime.error = "Please enter session time"
+                    return@setOnClickListener
+                } else if (TextUtils.isEmpty(et_recordCalories.text.toString())) {
+                    et_recordCalories.error = "Please enter number of calories burnt"
+                    return@setOnClickListener
+                }
 
-            startActivity(Intent(this@RecordActivity, HomeActivity::class.java))
-            finish()
-        }
+                currentUserDb?.child("Date")?.setValue(date.text.toString())
+                currentUserDb?.child("Weight")?.setValue(et_recordWeight.text.toString())
+                currentUserDb?.child("Time")?.setValue(et_recordTime.text.toString())
+                currentUserDb?.child("Calories")?.setValue(et_recordCalories.text.toString())
 
-        //return to home page when button pressed
-        btn_back.setOnClickListener {
-            startActivity(Intent(this@RecordActivity, HomeActivity::class.java))
-            finish()
+                startActivity(Intent(this@RecordActivity, HomeActivity::class.java))
+                finish()
+            }
+
+            //return to home page when button pressed
+            btn_back.setOnClickListener {
+                startActivity(Intent(this@RecordActivity, HomeActivity::class.java))
+                finish()
+            }
         }
     }
-}
